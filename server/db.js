8658d1221e58c7c431dc26d7e6d47782c4039284
Razi Kaabyia <rkaabyia@gmail.com>
2026-03-14 21:23:15 +0200

    fix database name

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: 'abuhisham_kitchen',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
